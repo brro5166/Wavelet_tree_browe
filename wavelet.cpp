94a84dc20a3bede8e822e681d4ea6f4c5e3ea5a2
brro5166 <brro5166@colorado.edu>
2024-12-08 19:47:28 -0700

    wavelet.cpp

---
+++ wavelet.cpp
@@ -0,0 +1,17 @@
+#contains the following functions
+
+#1 accepts an input string and assigns numbers to letters
+
+#2 Haar transformation
+
+#3 Quantization
+
+#4 Wavelet tree build
+
+#5 Wavelet tree insert
+
+#6 Wavelet tree traversal
+
+#7 Huffman encoding
+
+#8 Print compressed string
